3832014984d0a354c88135b28a149851093611b6
Bobbert <Robert.pallesen13@gmailcom>
2023-11-20 23:27:18 +0100

    todos

--- customer-service/src/main/java/com/turkeycrew/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/turkeycrew/CustomerServiceApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerServiceApplication {
 
+    // TODO: Setup topics for kafka when an api is called
+    // TODO: Folders?
+    // TODO: Test all apis and its fucntions
+    // TODO: Frontend?
+    // TODO: Make sure that the authentication works
+    // TODO: Fix error handling on apis
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
