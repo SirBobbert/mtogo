6f65324cf597da5ac237e3b9ce251ca25fb25160
Bobbert <Robert.pallesen13@gmailcom>
2023-12-05 10:11:12 +0100

    test

--- order-service/src/test/java/integration/OrderControllerIntegrationTest.java
+++ order-service/src/test/java/integration/OrderControllerIntegrationTest.java
@@ -1,15 +1,24 @@
 package integration;
 
-import com.turkeycrew.OrderService;
-import com.turkeycrew.OrderServiceApplication;
+import com.turkeycrew.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.http.*;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = OrderServiceApplication.class)
@@ -23,50 +32,47 @@ public class OrderControllerIntegrationTest {
     @MockBean
     private OrderService orderService;
 
-    @Mock
-    private KafkaTemplate<String, String> kafkaTemplate;
+    @Test
+    @Rollback
+    public void test_placeOrder() {
+        String url = "http://localhost:" + port + "/api/orders/create/{restaurantId}";
 
-//    @Test
-//    @Rollback
-//    public void test_placeOrder() {
-//        String url = "http://localhost:" + port + "/api/orders/create/{restaurantId}";
-//
-//        Order orderRequest = new Order();
-//        orderRequest.setUserId(1);  // Set the user ID
-//        // Add items to the order (you may need to adjust this based on your Order class structure)
-//        List<OrderItem> items = new ArrayList<>();
-//
-//        OrderItem item1 = new OrderItem().builder()
-//                .itemName("Item1")
-//                .quantity(2)
-//                .price(5.0)
-//                .totalPrice(2 * 5) // You may want to calculate the total price based on quantity and price
-//                .build();
-//
-//        OrderItem item2 = OrderItem.builder()
-//                .itemName("Item2")
-//                .quantity(1)
-//                .price(3.0)
-//                .totalPrice(1 * 3.0)
-//                .build();
-//
-//        items.add(item1);
-//        items.add(item2);
-//
-//        orderRequest.setItems(items);
-//        orderRequest.setTotalAmount(items.get(0).getTotalPrice() + items.get(1).getTotalPrice());
-//        orderRequest.setStatus(OrderStatus.PENDING);
-//
-//
-//        // Mock the behavior of the orderService.processOrder method
-//        when(orderService.processOrder(1, orderRequest)).thenReturn(ResponseEntity.ok("Order processed successfully"));
-//
-//        ResponseEntity<String> response = restTemplate.postForEntity(url, orderRequest, String.class, 1);
-//
-//        // Assert statements
-//        assertEquals(HttpStatus.CREATED.value(), response.getStatusCodeValue());
-//        assertEquals("Order processed successfully", response.getBody());
-//    }
+        Order orderRequest = new Order();
+        orderRequest.setUserId(1);  // Set the user ID
+        // Add items to the order (you may need to adjust this based on your Order class structure)
+        List<OrderItem> items = new ArrayList<>();
+
+        OrderItem item1 = new OrderItem().builder()
+                .itemName("Item1")
+                .quantity(2)
+                .price(5.0)
+                .totalPrice(2 * 5) // You may want to calculate the total price based on quantity and price
+                .build();
+
+        OrderItem item2 = OrderItem.builder()
+                .itemName("Item2")
+                .quantity(1)
+                .price(3.0)
+                .totalPrice(1 * 3.0)
+                .build();
+
+        items.add(item1);
+        items.add(item2);
+
+        orderRequest.setItems(items);
+        orderRequest.setTotalAmount(items.get(0).getTotalPrice() + items.get(1).getTotalPrice());
+        orderRequest.setStatus(OrderStatus.PENDING);
+
+
+        // Mock the behavior of the orderService.processOrder method
+        when(orderService.processOrder(1, orderRequest)).thenReturn(ResponseEntity.ok("Order processed successfully"));
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, orderRequest, String.class, 1);
+
+        // Assert statements
+        assertEquals(HttpStatus.CREATED.value(), response.getStatusCodeValue());
+        assertEquals("Order processed successfully", response.getBody());
+    }
 //
 //    @Test
 //    @Rollback
